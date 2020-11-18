8e47bea0b79230cc638ea9651aac1f0bb4fe8534
Alex Angelico <alex.angelico@live.com>
2020-11-17 17:05:29 -0800

    working on connecting database

---
+++ data/books.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+	id SERIAL PRIMARY KEY,
+	author VARCHAR (255),
+  title VARCHAR (255),
+	isbn INTEGER,
+	image_url VARCHAR (255),
+  description TEXT
+);
---
+++ data/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO books
+VALUES ('1, ')
+VALUES ('2, ');
